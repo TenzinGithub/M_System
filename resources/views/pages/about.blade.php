1137fca99882e8511fc1aaac3c3c1c5c107041ed
tnorbu <tnorbu@dit.gov.bt>
2019-05-12 13:20:48 -0700

    source code added

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,12 @@
+@extends('template.master')
+
+@section('content')
+<html>
+ <head>
+  <title>About Page</title>
+ </head>
+  <body>
+   <h1> Welcome to about Page </html>
+   </body>
+</html>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/table', function () {
+    return view('table');
+});
+
+Route::get('about',[
+    'as' => 'about', 
+    'uses' => 'PagesController@getAbout'
+]);
+
+
+Route::get('contact',[
+    'as' => 'contact', 
+'uses' => 'PagesController@getContact'
+]);
+
+
+Route::get('staffinfo',[
+    'as' => 'staffinfo', 
+'uses' => 'PagesController@getStaffinfo'
+]);
+
+Route::get('studentinfo',[
+    'as' => 'studentinfo', 
+'uses' => 'PagesController@getStudentinfo'
+]);
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
