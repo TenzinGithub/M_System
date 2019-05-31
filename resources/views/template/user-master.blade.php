c2a0dbe65444e7cd961db252c687a9317b033a95
tnorbu <tnorbu@dit.gov.bt>
2019-05-31 09:16:02 -0700

    added new code

---
+++ resources/views/template/user-master.blade.php
@@ -0,0 +1,27 @@
+
+<html style="height: auto; min-height: 100%;">
+<!-- jQuery 3 -->
+@include('template.user-template.head')
+<!-- ADD THE CLASS layout-top-nav TO REMOVE THE SIDEBAR. -->
+<body class="skin-blue layout-top-nav" style="height: auto; min-height: 100%;">
+<div class="wrapper" style="height: auto; min-height: 100%;">
+  <!-- Full Width Column -->
+  <div class="content-wrapper" style="min-height: 256px;">
+    <div class="container">
+      <!-- Content Header (Page header) -->
+      @include('template.user-template.header')
+      <section class="content-header">
+        <h1>
+          Welcome To Dashboard
+         
+        </h1>
+        
+      </section>
+    </div>
+</div>
+  @include('template.user-template.footer')
+</div>
+<<!-- jQuery 3 -->
+   @include('template.user-template.script')
+</body>
+</html>
---
+++ resources/views/template/user-template/header.blade.php
@@ -0,0 +1,60 @@
+
+  <header class="main-header">
+    <nav class="navbar navbar-static-top">
+      <div class="container">
+        <div class="navbar-header">
+          <a href="{{ asset('../../index2.html" class="navbar-brand') }}"><b>Management</b>System</a>
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
+            <i class="fa fa-bars"></i>
+          </button>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse pull-left" id="navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home <span class="sr-only">(current)</span></a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">About Us <span class="caret"></span></a>
+              <ul class="dropdown-menu" role="menu">
+                <li><a href="#">About System</a></li>
+                <li><a href="#">Mission and Vision</a></li>
+              </ul>
+            </li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Teacher_Register <span class="caret"></span></a>
+              <ul class="dropdown-menu" role="menu">
+                <li><a href="#">Teaching_Staff</a></li>
+                <li><a href="#">Non-Teaching_Staff</a></li>
+              </ul>
+            </li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Student_Register <span class="caret"></span></a>
+              <ul class="dropdown-menu" role="menu">
+                <li><a href="#">Boader_Students</a></li>
+                <li><a href="#">DaySchool_Student</a></li>
+              </ul>
+            </li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Details_List <span class="caret"></span></a>
+              <ul class="dropdown-menu" role="menu">
+                <li><a href="#">Staff</a></li>
+                <li><a href="#">Students</a></li>
+              </ul>
+            </li>
+            <li><a href="#">Announcement</a></li>
+            <li><a href="#">Gallery</a></li>
+            <li><a href="#">Contact Us</a></li>
+          </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+        <!-- Navbar Right Menu -->
+        <div class="navbar-custom-menu">
+          <ul class="nav navbar-nav">
+            
+          </ul>
+        </div>
+        <!-- /.navbar-custom-menu -->
+      </div>
+      <!-- /.container-fluid -->
+    </nav>
+  </header>
